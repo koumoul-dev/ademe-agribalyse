a114acfd60e4e2877e8968185a9d6a0253ae1c3d
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2023-07-21 09:04:23 +0200

    feat: updates for 3.1.1 version

--- data/generateCSV.js
+++ data/generateCSV.js
@@ -6,18 +6,19 @@ const stringify = require('csv-stringify/lib/sync')
 
 const fileName = 'AGRIBALYSE3.1_produits alimentaires_2.xlsm'
 
-const workbook = XLSX.readFile(path.join(__dirname, 'in', fileName))
 fs.ensureDirSync(path.join(__dirname, 'out'))
 
 console.log('generating synthesis CSV')
-const synthesisWS = workbook.Sheets[workbook.SheetNames[1]]
+const sWorkbook = XLSX.readFile(path.join(__dirname, 'in', 'AGRIBALYSE3.1.1_produits alimentaires - synthese.xlsx'))
+const synthesisWS = sWorkbook.Sheets[sWorkbook.SheetNames[0]]
 let csvToks = XLSX.utils.sheet_to_csv(synthesisWS, {FS:';', rawNumbers: true}).replace(/\r\n/g, ' ').split('\n')
 const synthesisHeaders = stringify([parse(csvToks[2], {delimiter: ';'})[0].slice(0,12).map(d => d.split(' (')[0])]).split(' - ')[0]+','+ stringify([parse(csvToks[1], {delimiter: ';'})[0].slice(12).map(d => d.replace(' 3.1', ''))])
 const synthesisCsvString = synthesisHeaders+stringify(parse(csvToks.slice(3).filter(l => l.length > 26).join('\n'), {delimiter: ';'}))
-fs.writeFileSync(path.join(__dirname, 'out','Agribalyse_' + workbook.SheetNames[1]+'.csv'), synthesisCsvString)
+fs.writeFileSync(path.join(__dirname, 'out','Agribalyse_' + sWorkbook.SheetNames[0]+'.csv'), synthesisCsvString)
 
 console.log('generating steps details CSV')
-const stepsWS = workbook.Sheets[workbook.SheetNames[2]]
+const eWorkbook = XLSX.readFile(path.join(__dirname, 'in', 'AGRIBALYSE3.1.1_produits alimentaires - detail etapes.xlsx'))
+const stepsWS = eWorkbook.Sheets[eWorkbook.SheetNames[0]]
 csvToks = XLSX.utils.sheet_to_csv(stepsWS, {FS:';', rawNumbers: true}).replace(/\r\n/g, ' ').split('\n')
 const stepsCategories = parse(csvToks[0], {delimiter: ';'})[0].filter(f => f.length).slice(1,-1).map(f => f.split(' 3.1')[0])
 stepsCategories.push('DQR')
@@ -30,13 +31,14 @@ stepsHeaders.forEach((f, i) => {if(f.split(' - ').pop() === 'Total') excludes.pu
 const stepsCsvStringHeader = stringify([stepsHeaders.filter((s, i) => !excludes.includes(i))])
 const stepsCsvStringBody = stringify(csvToks.slice(3).map(l => l.split(';').filter((s, i) => !excludes.includes(i)).map(s => s !== '-' ? s : '')).filter(l => l.length > 101))
 const stepsCsvString = stepsCsvStringHeader + stepsCsvStringBody
-fs.writeFileSync(path.join(__dirname, 'out','Agribalyse_' + workbook.SheetNames[2]+'.csv'), stepsCsvString)
+fs.writeFileSync(path.join(__dirname, 'out','Agribalyse_' + eWorkbook.SheetNames[0]+'.csv'), stepsCsvString)
 
 console.log('generating ingredients details CSV')
-const ingredientsWS = workbook.Sheets[workbook.SheetNames[3]]
+const iWorkbook = XLSX.readFile(path.join(__dirname, 'in', 'AGRIBALYSE3.1.1_produits alimentaires - detail ingredients.xlsx'))
+const ingredientsWS = iWorkbook.Sheets[iWorkbook.SheetNames[0]]
 const sheetCsv = XLSX.utils.sheet_to_csv(ingredientsWS, {FS:';', rawNumbers: true})
 csvToks = sheetCsv.replace(/\r\n/g, ' ').split('\n').slice(2)
 const ingredientsHeaders = [].concat(parse(csvToks[1], {delimiter: ';'})[0].slice(0,10),parse(csvToks[0], {delimiter: ';'})[0].slice(10, 27).map(f => f.split(' 3.1')[0]))
 const lines = csvToks.slice(2).map(r => r.split(';').slice(0, 27)).filter(line => line.length === 27 && line[6] !== 'Total')
 const ingredientsCsvString = stringify([ingredientsHeaders.filter((h, i) => i<7 || i > 9)]) + stringify(lines.map(line => line.filter((d, i) => i<7 || i > 9).map(s => s !== '-' ? s : '')))
-fs.writeFileSync(path.join(__dirname, 'out','Agribalyse_' + workbook.SheetNames[3]+'.csv'), ingredientsCsvString)
+fs.writeFileSync(path.join(__dirname, 'out','Agribalyse_' + iWorkbook.SheetNames[0]+'.csv'), ingredientsCsvString)
